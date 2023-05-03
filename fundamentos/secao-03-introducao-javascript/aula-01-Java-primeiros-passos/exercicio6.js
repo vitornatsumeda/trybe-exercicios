86409999d3190ff06cf5ad962496b4858e424c14
Vitor Natsumeda <vitor.natsumeda01@gmail.com>
2023-05-03 16:43:54 -0300

    Exercicios da plataforma

---
+++ fundamentos/secao-03-introducao-javascript/aula-01-Java-primeiros-passos/exercicio6.js
@@ -0,0 +1,14 @@
+let statusPS = "reprovada";
+switch (statusPS) {
+    case (statusPS = "aprovada"):
+    console.log("Parabéns, você está no grupo de pessoas aprovadas!");
+    break
+    case (statusPS = "lista"):
+    console.log ("Você está na nossa lista de espera.");
+    break
+    case (statusPS = "reprovada"):
+    console.log ("Infelizmente, você reprovou.");
+    break
+    default:
+    console.log ("Informação incorreta.")
+}
