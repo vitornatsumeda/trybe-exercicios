cac5eed8f2ad9d07402d58d0973d846ce1f8e9bb
Vitor Natsumeda <vitor.natsumeda01@gmail.com>
2023-05-04 16:26:48 -0300

    Criando pastas e resolvendo exercicios

---
+++ fundamentos/secao-03-introducao-javascript/aula-01-Java-primeiros-passos/Compilado-exercicios.js
@@ -0,0 +1,113 @@
+// Exercicio 1 
+let a = 12
+let b = 20
+let c = 11
+
+// console.log("O valor da adicao é", a+b)
+// console.log("O valor da subtracao é", a-b)
+// console.log("O valor da multiplicacao é", a*b)
+// console.log("O valor da divisao é", a/b)
+// console.log("O valor do modulo é", a%b)
+
+//------------------------------------------------------------------------
+
+// Exercicio 2
+// if (a > b) {
+//     console.log ("Valor de A é maior que B.");
+// } else {
+//     console.log ("Valor de B é maior que A.")
+// }
+
+//------------------------------------------------------------------------
+
+// Exercicio 3
+// if (a > b && a > c) {
+//     console.log ("A eh o maior valor");
+// } else if (b > a && b > c) {
+//     console.log ("B eh o maior valor");
+// } else {
+//     console.log ("C eh o maior valor");
+// }
+
+//------------------------------------------------------------------------
+
+// Exercicio 4
+// let d;
+
+// if (d >0) {
+//     console.log ("Positivo");
+// } else if (d < 0) {
+//     console.log ("Negative");
+// } else {
+//     console.log("Zero");
+// }
+
+//------------------------------------------------------------------------
+
+// Exercicio 5
+// let ang1 = 60
+// let ang2 = 60
+// let ang3 = -60
+// let angTotal = ang1 + ang2 + ang3
+// let angPositivos = ang1 > 0 && ang2 > 0 && ang3 > 0
+
+// if (angPositivos) {
+//     if (angTotal === 180) {
+//         console.log("True");
+//     }
+//     else {
+//         console.log("False");
+//     }
+// }
+// else {
+//     console.log("Angulo Invalido")
+// }
+
+//------------------------------------------------------------------------
+
+// Exercicio 6
+// let piece;
+
+// switch (piece.toLocaleLowerCase()) {
+//     case (piece = "pawn"):
+//         console.log("One space forward");
+//     break
+//     case (piece = "bishop"):
+//         console.log("Diagonal");
+//     break
+//     case (piece = "rook"):
+//         console.log("Vertical or Horizontal");
+//     break
+//     case (piece = "knight"):
+//         console.log("L-shape");
+//     break
+//     case (piece = "queen"):
+//         console.log("Vertical or Horizontal or Diagonal");
+//     break
+//     case (piece = "king"):
+//         console.log("One space everywhere");
+//     break
+//     default:
+//         console.log("Invalid piece");
+// }
+
+//------------------------------------------------------------------------
+
+// Exercicio 7
+// let note = 87
+
+// if (note >= 90 && note <= 100){
+//     console.log("Nota A");
+// } else if (note >= 80 && note < 90){
+//     console.log("Nota B");
+// } else if (note >= 70 && note < 80){
+//     console.log("Nota C");
+// } else if (note >= 60 && note < 70){
+//     console.log("Nota D");
+// } else if (note >= 50 && note < 60){
+//     console.log("Nota E");
+// } else if (note >= 0 && note < 50){
+//     console.log("Nota F");
+// } else {
+//     console.log ("Invalid Note")
+// }
