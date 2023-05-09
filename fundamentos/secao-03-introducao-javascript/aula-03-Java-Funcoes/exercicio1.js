5f91e2776484bda5023dbb3aa789bcbe2e9daa9a
Vitor Natsumeda <vitor.natsumeda01@gmail.com>
2023-05-09 02:16:28 -0300

    Fazendo exercicios da plataforma

---
+++ fundamentos/secao-03-introducao-javascript/aula-03-Java-Funcoes/exercicio1.js
@@ -0,0 +1,20 @@
+let saldo = 1000;
+// function AdicionaValor(valoradicionado){
+//     return valoradicionado + saldo
+// } 
+// console.log('O saldo é', AdicionaValor(500), 'reais')
+
+// function RetiraValor(valorretirado){
+//     return saldo - valorretirado
+// } 
+// console.log('O saldo é', RetiraValor(400), 'reais')
+
+// function TaxaValor(taxaAplicada){
+//     return saldo * (1 + taxaAplicada)
+// } 
+// console.log('O saldo é', TaxaValor(0.2), 'reais')
+
+function DividaValor(valorDividido){
+    return saldo/valorDividido
+} 
+console.log('O saldo é', DividaValor(2), 'reais')
