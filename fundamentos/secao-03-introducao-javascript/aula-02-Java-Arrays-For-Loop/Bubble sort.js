d903da824249810aa445013f7b7ddc18a31d958a
Vitor Natsumeda <vitor.natsumeda01@gmail.com>
2023-05-11 01:07:11 -0300

    Organizando Arquivos

---
+++ fundamentos/secao-03-introducao-javascript/aula-02-Java-Arrays-For-Loop/Bubble sort.js
@@ -0,0 +1,61 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//       if (numbers[index] < numbers[secondIndex]) {
+//         let position = numbers[index];
+//         numbers[index] = numbers[secondIndex];
+//         numbers[secondIndex] = position;
+//       }
+//     }
+//   } console.log(numbers)
+
+//------------------------------------------------------------------------------
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//       if (numbers[index] > numbers[secondIndex]) {
+//         let position = numbers[index];
+//         numbers[index] = numbers[secondIndex];
+//         numbers[secondIndex] = position;
+//       }
+//     }
+//   } console.log(numbers)
+
+//------------------------------------------------------------------------------
+
+// let newNumbers = [];
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (index < (numbers.length - 1)) {
+//         newNumbers.push(numbers[index] * numbers[index + 1]);
+//     } else {
+//         newNumbers.push(numbers[index] * 2 );
+//     }
+// } console.log(newNumbers);
+
+//------------------------------------------------------------------------------
+
+// let n = 5;
+// let symbol = "*"
+// let inputLine = '';
+
+// for (index = 0; index < n; index +=1){
+//     inputLine = inputLine + symbol
+// }
+// for (index = 0; index < n; index +=1 ){
+//     console.log(inputLine);
+// } console.log (inputLine)
+
+//------------------------------------------------------------------------------
+
+// let n = 5;
+// let symbol = "*";
+// let inputLine = '';
+
+// for (let index = 0; index <= n; index +=1) {
+//     console.log(inputLine);
+//     inputLine = inputLine + symbol;
+// };
+
+//------------------------------------------------------------------------------
+
