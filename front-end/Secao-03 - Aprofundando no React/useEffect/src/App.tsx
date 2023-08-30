0685c6797177083b07d5740954feee3e09726b22
Vitor Natsumeda <vitor.natsumeda01@gmail.com>
2023-08-30 19:51:52 -0300

    Exercicio da Plataforma

---
+++ front-end/Secao-03 - Aprofundando no React/useEffect/src/App.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import { fetchCoordinates } from './service';
+import './App.css'
+
+type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
+
+function App() {
+  const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCoordinates();
+      setCoordinates({ latitude: data.latitude, longitude: data.longitude });
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+    <h1>International Space Station Location Tracker</h1>
+    {coordinates && (
+      <>
+        <h2>{`Latitude: ${coordinates.latitude}`}</h2>
+        <h2>{`Longitude: ${coordinates.longitude}`}</h2>
+      </>
+    )}
+    </>
+  );
+}
+
+export default App
